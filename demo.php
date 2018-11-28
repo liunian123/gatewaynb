6ecc057d15ed2d592fa8dec8a6eaaf86145791e0
1483213287 <1483213287@qq.com>
2018-11-28 13:17:21 +0800

    提交第一个版本

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/28
+ * Time: 12:58
+ */
+
+require  'vendor/autoload.php';
+use workerman\gatewayworker\Gateway;
+Gateway::hello();
---
+++ src/workerman/gatewayworker/Gateway.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/28
+ * Time: 10:47
+ */
+
+namespace workerman\gatewayworker;
+
+class Gateway
+{
+
+    public function __construct()
+    {
+    }
+
+    public static function hello(){
+
+        echo "hello";
+    }
+
+
+}
